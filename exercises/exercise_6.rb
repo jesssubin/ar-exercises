67bbc9be41d9f80f07f34211245ca8f3f3af6652
jesssubin <jess.s.moon@gmail.com>
2020-11-10 00:28:54 +0000

    assignment completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessica", last_name: "Moon", hourly_rate: 65)
+@store1.employees.create(first_name: "Coco", last_name: "Chanel", hourly_rate: 62)
+
+@store2.employees.create(first_name: "Christian", last_name: "Dior", hourly_rate: 61)
+@store2.employees.create(first_name: "Yves", last_name: "St.Laurant", hourly_rate: 63)
+@store2.employees.create(first_name: "Louis", last_name: "Vuitton", hourly_rate: 64)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please provide the store name"
+puts ">"
+
+user_input = gets.chomp
+new_store = Store.new 
+new_store.name = user_input 
+
+store_name.errors.messages.each {|err|
+puts "Error: #{err} "}
